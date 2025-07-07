9bf01af8584820e83036acac45bc1a50969a90a9
rushipawar858 <rushipawar5207@gmail.com>
2025-07-07 19:41:47 +0530

    update code with listobjects

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const multer = require('multer');
-const { S3Client, PutObjectCommand, GetObjectCommand } = require('@aws-sdk/client-s3');
+const { S3Client, PutObjectCommand, GetObjectCommand ,ListObjectsV2Command } = require('@aws-sdk/client-s3');
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
+const { ListObjectsV2Command } = require('@aws-sdk/client-s3');
 const fs = require('fs');
 const app = express();
 const upload = multer({ dest: 'uploads/' });
